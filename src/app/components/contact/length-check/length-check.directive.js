13bec8d076771644b822de24c296093e7bc08d59
Roland Oben <roland@percy3d.com>
2017-09-10 16:29:42 -0400

    Created the functionality of of the length-check directive and added useful comments.

--- src/app/components/contact/length-check/length-check.directive.js
+++ src/app/components/contact/length-check/length-check.directive.js
@@ -1,12 +1,14 @@
+// Create a directive called 'lengthCheck' and register it under  the 'component.contact' module
+// This directive will be used to manipulate the DOM, just by making the input field transparent.
 function lengthCheck() {
   return {
     restrict: 'A',
-    require: 'ngModel',
+    require: 'ngModel', // require the ngModel in the link function so as to bind to the parent scope.
     compile: function ($element) {
-      $element.addClass('dynamic-input');
-      return function ($scope, $element, $attrs, $ctrl) {
+      $element.addClass('dynamic-input'); // A class is added to the element before it gets rendered, and as such will be added only once.
+      return function ($scope, $element, $attrs, $ctrl) { // Returns the link function that gets run after the directive has been created.
         var dynamicClass = 'dynamic-input--no-contents';
-        $scope.$watch(function () {
+        $scope.$watch(function () { // Watches the current input value, and if the value changed, will remove the 'dynamic-input' class, else will keep it.
           return $ctrl.$viewValue;
         }, function (newValue) {
           if (newValue) {
